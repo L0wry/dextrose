251dd987ac0df8fcebd60e285fa88a717b7ef122
Rohan Janjua <rohan.janjua@gmail.com>
2017-11-28 16:23:08 +0000

    fix: snapshots are named correctly

--- src/snap-batcher.js
+++ src/snap-batcher.js
@@ -1,6 +1,7 @@
 import log from "./logger";
 
 export default async(dextrose, snapPath, teardown) => {
+
   try {
     const componentsLoaded = await dextrose.client.getLoadedComponents();
 
@@ -8,9 +9,10 @@ export default async(dextrose, snapPath, teardown) => {
 
     for (let i = 0; i < componentsLoaded.length; i++) {
       await dextrose.client.loadComponent(componentsLoaded[i]);
-      await dextrose.snapper.snap(`${snapPath}/${componentsLoaded[i]}`)
+      const outputName = componentsLoaded[i].replace(/\s/g, "_").replace(/[\[\]\\+.,\/#!$%\^&\*;:{}=\-`'~()]/g,"");  
+      await dextrose.snapper.snap(`${snapPath}/${outputName}`)
 
-      log.info('snapBatcher', `Snapped component: ${componentsLoaded[i]}`)
+      log.info('snapBatcher', `Snapped component: ${outputName}`)
     }
   } catch (err) {
     throw (err)
